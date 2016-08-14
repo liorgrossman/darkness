53bc9a64d001767054288dd2dc4d3e0a3db45c77
Lior Grossman <lior@liorgrossman.com>
2016-08-14 19:48:44 +0300

    Fixed exception in PayPal check

--- chrome-extension/js/background.js
+++ chrome-extension/js/background.js
@@ -127,7 +127,7 @@ var reloadUser = function(callback) {
 	} else {
 		// Check Google Payments
 		checkGooglePayments(function(err, type) {
-			if (ENVIRONMENT == 'staging') type = true;
+			// if (ENVIRONMENT == 'staging') type = true;
 			if (err) {
 				logError('checkGooglePayments error: ', err);
 			} else { // no error
@@ -295,7 +295,7 @@ var notifyOnPayPalTransactionComplete = function(callbackToNotifyClient) {
 			// Don't even send a response to client, keep the dialog open...
 		} else {
 			setTimeout(function() {
-				reloadPayPalUserUntilSuccess(callbackToNotifyClient);
+				notifyOnPayPalTransactionComplete(callbackToNotifyClient);
 			}, 1000);
 		}
 	}
