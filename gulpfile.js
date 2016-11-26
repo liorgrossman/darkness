f98e9f54c7b5ec0882816c7f6de489d3b3c6d138
Lior Grossman <lior@liorgrossman.com>
2016-11-26 15:16:18 +0200

    Some improvements to auto-creation of new skins Code more resilient to syntax errors, more instructions

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,17 @@ const chromeZipsDir = 'chrome-zips';
 var manifestJson = JSON.parse(fs.readFileSync(chromeDevelopmentDir + '/manifest.json'));
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
+// Helper methods
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+function fileExists(filePath) {
+	try {
+		return fs.statSync(filePath).isFile();
+	} catch (err) {
+		return false;
+	}
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------
 // Preapre a ZIP that is uploaded to Chrome Web Store:
 // https://chrome.google.com/webstore/detail/darkness-beautiful-dark-t/imilbobhamcfahccagbncamhpnbkaenm
 //----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -24,7 +35,7 @@ var manifestJson = JSON.parse(fs.readFileSync(chromeDevelopmentDir + '/manifest.
 gulp.task('cws:replicate', function() {
 	gutil.log("Replicate started");
 	// Delete any existing production dir
-	var deletedFiles = del.sync([chromeProductionDir, chromeZipsDir + '/' + zipFilename], {force: true, dryRun: false});
+	var deletedFiles = del.sync([chromeProductionDir, chromeZipsDir + '/' + zipFilename], { force: true, dryRun: false });
 	for (var i in deletedFiles) gutil.log("Deleted: ", deletedFiles[i]);
 	// Copy dev dir recursively to production dir
 	return gulp.src([chromeDevelopmentDir + '/**/*']).pipe(gulp.dest(chromeProductionDir));
@@ -35,14 +46,14 @@ gulp.task('cws:cleanup', ['cws:replicate'], function() {
 	gutil.log("Cleanup started");
 	var deletedFiles = del.sync(
 		[chromeProductionDir + '/themes',
-		chromeProductionDir + '/style'],
-		{force: true, dryRun: false});
+			chromeProductionDir + '/style'
+		], { force: true, dryRun: false });
 	for (var i in deletedFiles) gutil.log("Deleted directory: ", deletedFiles[i]);
 
 	deletedFiles = del.sync(
 		[chromeProductionDir + '/**/*.scss',
-			chromeProductionDir + '/**/*.map'],
-		{force: true, dryRun: false});
+			chromeProductionDir + '/**/*.map'
+		], { force: true, dryRun: false });
 	for (var i in deletedFiles) gutil.log("Deleted file: ", deletedFiles[i]);
 });
 
@@ -57,7 +68,7 @@ gulp.task('cws:zip', ['cws:cleanup'], function() {
 
 // Zip the production directory
 gulp.task('cws:archive', ['cws:zip'], function() {
-	var archiveFilename = 'Darkness-CWS-v' + manifestJson.version + '_(' + (new Date()).toISOString().slice(0,19).replace('T', '__').replace(/:/g, '-') + ').zip';
+	var archiveFilename = 'Darkness-CWS-v' + manifestJson.version + '_(' + (new Date()).toISOString().slice(0, 19).replace('T', '__').replace(/:/g, '-') + ').zip';
 	gutil.log("Copying to archive: " + archiveFilename);
 	// Copy to archive
 	return gulp.src(chromeZipsDir + '/' + zipFilename).pipe(rename(archiveFilename)).pipe(gulp.dest(chromeZipsDir));
@@ -73,14 +84,13 @@ gulp.task('cws', ['cws:replicate', 'cws:cleanup', 'cws:zip', 'cws:archive']);
 gulp.task('sass:cleanup', function() {
 	gutil.log("Cleaning up .css and .css.map files");
 	var deletedFiles = del.sync([
-			chromeDevelopmentDir + '/themes/**/*.css',
-			chromeDevelopmentDir + '/themes/**/*.css.map',
-			chromeDevelopmentDir + '/themes-css',
-			chromeDevelopmentDir + '/style/**/*.css',
-			chromeDevelopmentDir + '/style/**/*.css.map',
-			chromeDevelopmentDir + '/style-css'
-		],
-		{force: true, dryRun: false});
+		chromeDevelopmentDir + '/themes/**/*.css',
+		chromeDevelopmentDir + '/themes/**/*.css.map',
+		chromeDevelopmentDir + '/themes-css',
+		chromeDevelopmentDir + '/style/**/*.css',
+		chromeDevelopmentDir + '/style/**/*.css.map',
+		chromeDevelopmentDir + '/style-css'
+	], { force: true, dryRun: false });
 	for (var i in deletedFiles) gutil.log("Deleted: ", deletedFiles[i]);
 });
 
@@ -125,26 +135,46 @@ gulp.task('install', function() {
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 // New Skin
 //----------------------------------------------------------------------------------------------------------------------------------------------------
-var argv = require('yargs').argv;
-var replace = require('gulp-replace');
-//example:
-// gulp newSkin --skin=stackoverflow
-gulp.task('newSkin', function() {
-	var skin = argv.skin;
-	gutil.log("start new skin: " +skin);
-	gutil.log("copy .scss file");
+
+// Create a new skin for Darkness
+// Example: gulp skin:create --name=stackoverflow
+gulp.task('skin:create', function() {
+	var argv = require('yargs').argv;
+	var replace = require('gulp-replace');
+	var skinKey = argv.key;
+	var errors = [];
+	if (!skinKey || skinKey.length == 0) errors.push("ERROR: [KEY] must be specified and cannot be empty, aborting...");
+	else if (!skinKey.match(/^[a-z0-9]+$/)) errors.push("ERROR: [KEY] must be all lowercase, no special characters allowed, aborting...");
+	else if (fileExists(chromeDevelopmentDir + '/themes/websites/' + skinKey + '.scss')) errors.push("ERROR: a skin named '" + skinKey + "' already exists, aborting...");
+
+	if (errors.length > 0) {
+		console.log("Usage: gulp skin:create --key=[KEY]\n" +
+			"This script creates a boilerplate for a new Darkness skin.\n" +
+			"[KEY] = a unique key for the website. Must be alphanumeric, all lowercase, no spaces or special characters are allowed.\n" +
+			"        Good examples: stackoverflow, cnn, 4chan, nytimes, googledrive\n" +
+			"        Bad examples: stack-overflow, CNN, 4Chan, NY_Times, Google Drive\n");
+		console.log("\n" + errors.join("\n") + "\n");
+		return;
+	}
+	// Duplicating WEBSITE-TEMPLATE.scss
 	gulp.src(chromeDevelopmentDir + '/themes/websites/WEBSITE-TEMPLATE.scss')
-		.pipe(rename({basename: skin}))
+		.pipe(rename({ basename: skinKey }))
 		.pipe(gulp.dest(chromeDevelopmentDir + '/themes/websites/'));
-	gutil.log("copy and modify [SITE]-iceberg.scss .scss file for skin");
+
+	// Copying and modifying [SITE]-iceberg.scss .scss file for skin
 	gulp.src(chromeDevelopmentDir + '/themes/google-*.scss')
-		.pipe(rename(function(path)
-		{
-			var res = path.basename.replace("google", skin);
+		.pipe(rename(function(path) {
+			var res = path.basename.replace("google", skinKey);
 			path.basename = res;
 			return path;
 		}))
-		.pipe(replace('google', skin))
+		.pipe(replace('google', skinKey))
 		.pipe(gulp.dest(chromeDevelopmentDir + '/themes/'));
-	
+
+	console.log("Skin '" + skinKey + "' successfully created!");
+	console.log("1. Please edit 'js/background/config.js' and add '" + skinKey + "' to CONFIG.sites, use '" + skinKey + "' as key");
+	console.log("2. Browse to chrome://extensions/ and reload the Darkness extension");
+	console.log("3. Edit your new skin: " + chromeDevelopmentDir + '/themes/websites/' + skinKey + '.scss');
+	console.log("4. To continuously compile the .scss during development, run: gulp sass:watch");
+
 });
